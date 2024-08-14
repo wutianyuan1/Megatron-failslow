504bca11728c54657364d839c1d84b6c7b899e4a
root <root@ip-172-31-44-91.ec2.internal>
2024-08-14 08:25:42 +0000

    initial megatron

---
+++ examples/pretrain_gpt_distributed_with_mp.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Runs the "345M" parameter model
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export OMP_NUM_THREADS=1
+export LD_PRELOAD=$LD_PRELOAD:/workspace/ncclprobe/build/libncclprobe.so
+export CONTROL_PLANE_WHL_PATH=/workspace/Megatron-LM/trainlogs/control_plane-1.0-py3-none-any.whl
+export NCCLPROBE_LOG_PATH=/workspace/Megatron-LM/trainlogs/
+export GLOBAL_CONTROLLER_LOG_PATH=/workspace/Megatron-LM/trainlogs/
+export LOCAL_CONTROLLER_LOG_PATH=/workspace/Megatron-LM/trainlogs/
+
+GPUS_PER_NODE=4
+# Change for multinode config
+MASTER_ADDR=localhost
+MASTER_PORT=6000
+NNODES=1
+NODE_RANK=0
+WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+
+CHECKPOINT_PATH=./checkpoints
+VOCAB_FILE=/workspace/dataset/gpt2-vocab.json
+MERGE_FILE=/workspace/dataset/gpt2-merges.txt
+DATA_PATH=/workspace/dataset/gpt2_text_document
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+GPT_ARGS="
+    --tensor-model-parallel-size 2\
+    --pipeline-model-parallel-size 2\
+    --sequence-parallel \
+    --num-layers 64 \
+    --hidden-size 2048 \
+    --num-attention-heads 32 \
+    --seq-length 32 \
+    --max-position-embeddings 1024 \
+    --micro-batch-size 4 \
+    --global-batch-size 16 \
+    --lr 0.00015 \
+    --train-iters 500000 \
+    --lr-decay-iters 320000 \
+    --lr-decay-style cosine \
+    --min-lr 1.0e-5 \
+    --weight-decay 1e-2 \
+    --lr-warmup-fraction .01 \
+    --clip-grad 1.0 \
+    --fp16
+"
+
+DATA_ARGS="
+    --data-path $DATA_PATH \
+    --vocab-file $VOCAB_FILE \
+    --merge-file $MERGE_FILE \
+    --split 949,50,1
+"
+
+OUTPUT_ARGS="
+    --log-interval 10 \
+    --save-interval 10000 \
+    --eval-interval 1000 \
+    --eval-iters 10
+"
+
+torchrun $DISTRIBUTED_ARGS pretrain_gpt.py \
+    $GPT_ARGS \
+    $DATA_ARGS \
+    $OUTPUT_ARGS \
+    --distributed-backend nccl \
+    --save $CHECKPOINT_PATH \
+    --load $CHECKPOINT_PATH
+
---
+++ tests/unit_tests/dist_checkpointing/models/test_gpt_model.py
@@ -0,0 +1,127 @@
+# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
+
+import pytest
+
+import torch
+from torch.distributed._tensor import DeviceMesh
+
+from megatron.core.dist_checkpointing import save, load, load_plain_tensors
+from megatron.core import parallel_state as ps
+from megatron.core.dist_checkpointing.dict_utils import diff
+from megatron.core.transformer.transformer_config import TransformerConfig
+from megatron.core.models.gpt.gpt_model import GPTModel
+from tests.unit_tests.dist_checkpointing import TempNamedDir
+from tests.unit_tests.test_utilities import Utils
+from megatron.core.tensor_parallel.random import model_parallel_cuda_manual_seed
+from megatron.core.models.gpt.gpt_layer_specs import \
+    get_gpt_layer_with_transformer_engine_spec as gpt_te_spec, get_gpt_layer_local_spec as gpt_local_spec
+
+
+def initialize_gpt_model(seed, layer_spec_fn=gpt_te_spec, **config_kwargs):
+    torch.manual_seed(seed)
+    model_parallel_cuda_manual_seed(seed)
+
+    default_config_kwargs=dict(num_layers=8, hidden_size=16, num_attention_heads=8, use_cpu_initialization=True)
+    default_config_kwargs.update(**config_kwargs)
+    transformer_config = TransformerConfig(**default_config_kwargs)
+    pre_process = ps.is_pipeline_first_stage()
+    post_process = ps.is_pipeline_last_stage()
+    model = GPTModel(config=transformer_config, transformer_layer_spec=layer_spec_fn(), vocab_size=128, max_sequence_length=4,
+                     pre_process=pre_process, post_process=post_process)
+
+    with torch.no_grad():
+        for p in model.parameters():
+            p.random_()
+    return model
+
+
+class TestGPTModel:
+    @pytest.mark.parametrize('src_layer_spec_fn', [gpt_te_spec, gpt_local_spec])
+    @pytest.mark.parametrize('dst_layer_spec_fn', [gpt_te_spec, gpt_local_spec])
+    def test_sharded_state_dict_save_load(self, tmp_path_dist_ckpt,
+                                          src_layer_spec_fn, dst_layer_spec_fn):
+        Utils.initialize_model_parallel(2,4)
+        gpt_model = initialize_gpt_model(1, src_layer_spec_fn)
+        with TempNamedDir(tmp_path_dist_ckpt / 'test_gpt_model') as ckpt_dir:
+            # Save
+            sharded_state_dict = gpt_model.sharded_state_dict()
+            save(sharded_state_dict, ckpt_dir)
+
+            # Load
+            gpt_model = initialize_gpt_model(2, dst_layer_spec_fn)
+            sharded_state_dict = gpt_model.sharded_state_dict()
+            state_dict = load(sharded_state_dict, ckpt_dir)
+            gpt_model.load_state_dict(state_dict)
+        Utils.destroy_model_parallel()
+
+
+class TestGPTModelReconfiguration:
+    @pytest.mark.parametrize("src_tp_pp,dest_tp_pp,src_layer_spec_fn,dst_layer_spec_fn", [
+        ((2, 4), (4, 2), gpt_te_spec, gpt_te_spec),
+        ((1, 8), (8, 1), gpt_te_spec, gpt_te_spec),
+        ((2, 1), (1, 8), gpt_te_spec, gpt_te_spec),
+        ((1, 1), (2, 2), gpt_te_spec, gpt_te_spec),
+        ((2, 1), (1, 8), gpt_local_spec, gpt_local_spec),
+        ((1, 1), (2, 4), gpt_te_spec, gpt_local_spec),
+        ((1, 8), (2, 1), gpt_local_spec, gpt_te_spec),
+    ])
+    def test_parallel_reconfiguration_e2e(self, tmp_path_dist_ckpt, src_tp_pp, dest_tp_pp,
+                                          src_layer_spec_fn, dst_layer_spec_fn):
+        """ Test model saving and loading with different TP/PP """
+        with TempNamedDir(tmp_path_dist_ckpt / 'test_gpt_model_reconfiguration_model_A') as ckpt_dir_A, \
+             TempNamedDir(tmp_path_dist_ckpt / 'test_gpt_model_reconfiguration_model_B') as ckpt_dir_B:
+            # Save checkpoint A
+            Utils.initialize_model_parallel(*src_tp_pp)
+            gpt_model_A = initialize_gpt_model(1, src_layer_spec_fn)
+            save(gpt_model_A.sharded_state_dict(), ckpt_dir_A)
+            regular_state_dict_A = gpt_model_A.state_dict()
+            Utils.destroy_model_parallel()
+
+            # Load checkpoint A with different TP/PP and save as checkpoint B
+            Utils.initialize_model_parallel(*dest_tp_pp)
+            gpt_model_B = initialize_gpt_model(2, dst_layer_spec_fn)
+            state_dict = load(gpt_model_B.sharded_state_dict(), ckpt_dir_A)
+            gpt_model_B.load_state_dict(state_dict)
+            save(gpt_model_B.sharded_state_dict(), ckpt_dir_B)
+            regular_state_dict_B = gpt_model_A.state_dict()
+            Utils.destroy_model_parallel()
+
+            # Test both checkpoints are equal
+            Utils.initialize_model_parallel(1, 1)
+            plain_state_dict_A = load_plain_tensors(ckpt_dir_A)
+            plain_state_dict_B = load_plain_tensors(ckpt_dir_B)
+            diffs = diff(plain_state_dict_A, plain_state_dict_B)
+            assert not any(map(bool, diffs)), diffs
+
+            # Test both regular state dicts are equal, turning FP8 states to bytes first
+            regular_state_dict_A = {k: v for k, v in regular_state_dict_A.items()
+                                    if not k.endswith('_extra_state')}
+            regular_state_dict_B = {k: v for k, v in regular_state_dict_B.items()
+                                    if not k.endswith('_extra_state')}
+            diffs = diff(regular_state_dict_A, regular_state_dict_B)
+            assert not any(map(bool, diffs)), diffs
+            Utils.destroy_model_parallel()
+
+
+    def test_state_dict_comparison(self, tmp_path_dist_ckpt):
+        Utils.initialize_model_parallel(2, 4)
+        with TempNamedDir(tmp_path_dist_ckpt / 'test_state_dict_comparison_A') as ckpt_dir_A, \
+             TempNamedDir(tmp_path_dist_ckpt / 'test_state_dict_comparison_B') as ckpt_dir_B:
+            gpt_model_A = initialize_gpt_model(1)
+            save(gpt_model_A.sharded_state_dict(), ckpt_dir_A)
+            gpt_model_B = initialize_gpt_model(2)
+            save(gpt_model_B.sharded_state_dict(), ckpt_dir_B)
+
+            state_dict_A = load_plain_tensors(ckpt_dir_A)
+            state_dict_A_dup = load_plain_tensors(ckpt_dir_A)
+            state_dict_B = load_plain_tensors(ckpt_dir_B)
+
+            # Test that A matches A
+            diffs = diff(state_dict_A, state_dict_A_dup)
+            assert not any(map(bool, diffs)), diffs
+
+            # Test that A *keys* match B *keys*, but the tensors content is different
+            only_left, only_right, mismatch = diff(state_dict_A, state_dict_B)
+            assert (not only_left and not only_right), (only_left, only_right)
+            assert len(mismatch) == len(state_dict_A), (len(mismatch), (len(state_dict_A)))
+        Utils.destroy_model_parallel()
