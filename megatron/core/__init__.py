9af758c9798455bdb3e40c714dee00f242f46b1e
Ubuntu <ubuntu@ip-172-31-16-231.ec2.internal>
2024-08-16 13:01:16 +0000

    new_dis

---
+++ megatron/core/__init__.py
@@ -0,0 +1,21 @@
+import megatron.core.tensor_parallel
+import megatron.core.utils
+from megatron.core import parallel_state
+from megatron.core.distributed import DistributedDataParallel
+from megatron.core.inference_params import InferenceParams
+from megatron.core.model_parallel_config import ModelParallelConfig
+from megatron.core.timers import Timers
+
+# Alias parallel_state as mpu, its legacy name
+mpu = parallel_state
+
+__all__ = [
+    "parallel_state",
+    "tensor_parallel",
+    "utils",
+    "logger",
+    "DistributedDataParallel",
+    "InferenceParams",
+    "ModelParallelConfig",
+    "Timers",
+]
